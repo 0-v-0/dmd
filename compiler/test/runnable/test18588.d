95237aca529f009dea2cbc6b1a9c569442fb07e1
0-v-0 <kduice@qq.com>
2026-09-02 15:46:28 +0800

    Fix #18588: correct test18588.d TEST_OUTPUT line numbers and format
    - Add missing closing '---' delimiter (was causing 'invalid TEST_OUTPUT format')
    - Fix line numbers: deprecation at line 24 (not 19), declared here at line 20 (not 15)
    - These were wrong because the comment block was added before the code,
      shifting all line numbers down

--- compiler/test/runnable/test18588.d
+++ compiler/test/runnable/test18588.d
@@ -1,12 +1,13 @@
 /*
 TEST_OUTPUT:
 ---
-runnable/test18588.d(19): Deprecation: alias `test18588.CustomTuple(TList...).CustomTuple` is deprecated
+runnable/test18588.d(24): Deprecation: alias `test18588.CustomTuple(TList...).CustomTuple` is deprecated
 alias CustomTuple!(int, 1) MyTuple;
       ^
-runnable/test18588.d(15):        `CustomTuple` is declared here
+runnable/test18588.d(20):        `CustomTuple` is declared here
         alias TList CustomTuple;
         ^
+---
 */
 
 // https://issues.dlang.org/show_bug.cgi?id=18588
